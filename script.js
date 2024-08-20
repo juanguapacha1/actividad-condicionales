9104d458063ccb72204867d739498a9d95309304
juan guapacha <juandavidguapachavillegas@gmail.com>
2024-08-20 15:39:29 -0500

    ejercicio 2

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ else{
 }
 }
 
+function ejercicio_2 () {
+    let numeroUsuario = prompt('Ingrese un numero')
+if (numeroUsuario >= 0){
+    console.log('El numero es positivo')
+}
+else{
+    console.log('El numero es negativo')
+}
+}
+
