1781caf4c78fe4e06f7b4a8e9248d42bedbbb038
juan guapacha <juandavidguapachavillegas@gmail.com>
2024-08-20 15:38:17 -0500

    ejercicio 1

---
+++ script.js
@@ -0,0 +1,10 @@
+function ejercicio_1 () {
+let edadUsuario = prompt('Ingresa tu edad')
+if (edadUsuario >= 18){
+    console.log('Eres mayor de edad')
+}
+else{
+    console.log('Eres menor de edad')
+}
+}
+
